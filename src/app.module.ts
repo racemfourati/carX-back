77e21cff7b64ad89e4216cd491af6559434890f3
mnawa3 <fouratir16@gmail.com>
2021-11-04 19:45:53 +0100

    conflict solved

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WorkersModule } from './workers/workers.module';
-<<<<<<< HEAD
 import PhoneController from './auth/phone/phone.controller';
 import { PhoneModule } from './auth/phone/phone.module';
 import { PhoneService } from './auth/phone/phone.service';
 import { CloudinaryModule } from './image/cloudinary/cloudinary.module';
-// import { Cloudinary } from './image/cloudinary/cloudinary.Provider';
-=======
-import { ReviewsModule } from './reviews/reviews.module';
-
-
-
-
->>>>>>> cb6e9a2b9a89523e3a775144df3ec5d8f83d06bf
 
 
 @Module({
@@ -30,31 +21,18 @@ import { ReviewsModule } from './reviews/reviews.module';
       url: process.env.DATABASE_URL ,
       autoLoadEntities: true,
       synchronize: true,
-<<<<<<< HEAD
     
-=======
-     
->>>>>>> cb6e9a2b9a89523e3a775144df3ec5d8f83d06bf
       extra: {
         ssl: true
       }
     }),
     UsersModule,
     WorkersModule,
-<<<<<<< HEAD
     PhoneModule,
     CloudinaryModule
   ],
   controllers: [AppController, PhoneController],
   providers: [AppService, PhoneService],
   
-=======
-    ReviewsModule,
-    
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-   
->>>>>>> cb6e9a2b9a89523e3a775144df3ec5d8f83d06bf
 })
 export class AppModule {}
