3eee3a14ab509a7c9047427ea6b8a03a19c0e0f1
firas-debich <debichf02@gmail.com>
2021-11-07 21:43:45 +0100

    phone auauth with phone done

--- src/users/users-controller/users-controller.controller.ts
+++ src/users/users-controller/users-controller.controller.ts
@@ -3,43 +3,116 @@ import { Users } from '../user.interface';
 import { UsersService } from '../users/users.service';
 import { Observable, Subscriber } from 'rxjs';
 import { Response } from 'express';
-
+var jwt= require("jsonwebtoken")
 import { FileInterceptor } from '@nestjs/platform-express'
 
 @Controller('users')
 export class UsersController {
+  val:any = Math.floor(100000 + Math.random() * 900000);
 
   constructor(private UsersService: UsersService) { }
+  ///auth with phone
+ @Post("send")
+  send( @Body() user: Users, @Res() respone: Response){
+    this.UsersService.getUserWithPhoneNumber(user).subscribe((result)=>{
+      console.log(result)
+      if(result.length ==0){
+        this.UsersService.add(user).then((result_)=>{
+          console.log(result_)
+          const welcomeMessage = `Welcome carX! Your verification code is ${this.val}`
+          let number = `+216${result_.phone}`
+          this.UsersService.sendSms(number,welcomeMessage)
+          const token =jwt.sign(
 
+            {user_id:result_.id},
+            process.env.TOKEN_KEY
+            
+          )
+          respone.status(HttpStatus.CREATED)
+          .json({ respond: "PHONE_NUMBER_NOT_FOUND", Token: token,verifCode:this.val }) 
+        })
+      }else{
+        const token =jwt.sign(
+
+          {user_id:result[0]},
+          process.env.TOKEN_KEY
+          
+        )
+          const welcomeMessage = `Welcome carX! Your verification code is ${this.val}`
+          let number = `+216${result[0].phone}`
+          this.UsersService.sendSms(number,welcomeMessage)
+          respone.status(HttpStatus.CREATED)
+          .json({ respond: "PHONE_NUMBER_WAS_FOUND", Token: token,verifCode:this.val })
+      }
+
+    })
+    
+  
+  }
+   
+///AUTH WITH GOOGLE
   @Post()
-  add(@Body() user: Users, @Res() respone: Response): any {
+   add(@Body() user: Users, @Res() respone: Response): any {
     this.UsersService.getOne(user).subscribe((result) => {
       if (result.length == 0) {
 
-        this.UsersService.add(user)
-        respone.status(HttpStatus.CREATED)
-          .json({ respond: "NOT FOUND", data: user })
+       this.UsersService.add(user).then((result) => {
+      
+        const token =jwt.sign(
+
+          {user_id:result.id},
+          process.env.TOKEN_KEY
+          
+        ) 
+         respone.status(HttpStatus.CREATED)
+         .json({ respond: "NOT FOUND", Token: token })
+       })
       } else if (result.length > 0) {
+        const token =jwt.sign(
+
+          {user_id:result[0].id},
+          process.env.TOKEN_KEY
+          
+        )
+        console.log(token)
         respone.status(HttpStatus.FOUND)
-          .json({ respond: "FOUND", data: result })
+          .json({ respond: "FOUND", Token: token })
       }
 
     })
   }
-
+//update just user 
     @Put('edit')
    
    async updateUser(@Body() user: Users  ){
-    
-  
-  
-      return this.UsersService.updateUser(user)
+     return this.UsersService.updateUser(user).then((result) =>
+     {
+       console.log(result)
+     })
     }
+    //get spesific user with id 
+    @Get("finduser")
+      findUser(@Body() user: Users,  @Res() respone: Response){
+       this.UsersService.getUerWithId(user).subscribe((result)=>{
+         console.log(result)
+         const token =jwt.sign(
 
+          {user_id:result[0].id},
+          process.env.TOKEN_KEY
+          
+        )
+        console.log(token)
+        respone.status(HttpStatus.FOUND)
+          .json({ respond: "FOUND", Token: token })
+       })
+      
+      }    
+      //get all user 
   @Get()
   findAll(): Observable<Users[]> {
     return this.UsersService.findAll()
   }
+  // update image 
     @Post('upload/:id')
     @UseInterceptors(FileInterceptor('file'))
    async uploadImage(@UploadedFile() file: Express.Multer.File ,@Param() params) {
--- src/users/users/users.service.ts
+++ src/users/users/users.service.ts
@@ -5,15 +5,47 @@ import { Repository, getRepository, createQueryBuilder, getConnection } from 'ty
 import { Users } from '../user.interface';
 import { from, Observable } from 'rxjs';
 import { CloudinaryService } from '../../image/cloudinary/cloudinary.service';
+require('dotenv').config();
+import { InjectTwilio, TwilioClient } from 'nestjs-twilio';
+import { throwError } from 'rxjs';
 @Injectable()
 export class UsersService {
+   accountSid = process.env.TWILIO_ACCOUNT_SID;
+ authToken = process.env.TWILIO_AUTH_TOKEN;
     constructor(
         @InjectRepository(userEntity)
         private userRepository: Repository<userEntity>
-        , private cloudinary: CloudinaryService
-    ) { }
+        , private cloudinary: CloudinaryService,
+      
+    ) {}
 
+        getUerWithId(user:Users): Observable<userEntity[]>{
+            return from(this.userRepository.find({
+                where: [
+                    { id: user.id }
+                ]
+            })) 
+        }
+        getUserWithPhoneNumber(user:Users): Observable<userEntity[]>{
+            return from(this.userRepository.find({
+                where: [
+                    { phone: user.phone }
+                ]
+            }))        
+        }
+    
 
+     sendSms (phone, message:any) {
+        const client = require('twilio')(this.accountSid, this.authToken);
+        client.messages
+          .create({
+             body: message,
+             from: process.env.TWILIO_PHONE_NUMBER,
+             to: phone
+           })
+          .then(message => console.log(message.sid));
+      }
+    
     getOne(user: Users): Observable<userEntity[]> {
 
 
